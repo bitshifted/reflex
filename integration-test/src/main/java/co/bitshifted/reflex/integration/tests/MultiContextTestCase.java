ceaecec869394757d5e5e31df5ae445fef1375b2
Vladimir Djurovic <vdjurovic@bitshifted.co>
2025-07-24 11:35:43 +0200

    chore: update license headers

--- integration-test/src/main/java/co/bitshifted/reflex/integration/tests/MultiContextTestCase.java
+++ integration-test/src/main/java/co/bitshifted/reflex/integration/tests/MultiContextTestCase.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright © 2024-2025, Bitshift D.O.O <https://bitshifted.co>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package co.bitshifted.reflex.integration.tests;
 
 import static co.bitshifted.reflex.core.Reflex.client;
