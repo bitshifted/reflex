dd41dbd7e11d1d0d7d9da8ac2479c37debcb568e
Vladimir Djurovic <vdjurovic@bitshifted.co>
2025-07-24 13:04:01 +0200

    chore: update license headers (#95)

--- reflex-core/src/main/java/co/bitshifted/reflex/core/config/RFXRedirectPolicy.java
+++ reflex-core/src/main/java/co/bitshifted/reflex/core/config/RFXRedirectPolicy.java
@@ -1,13 +1,10 @@
 /*
+ * Copyright © 2024-2025, Bitshift D.O.O <https://bitshifted.co>
  *
- *  * Copyright (c) 2023-2023  Bitshift D.O.O (http://bitshifted.co)
- *  *
- *  * This Source Code Form is subject to the terms of the Mozilla Public
- *  * License, v. 2.0. If a copy of the MPL was not distributed with this
- *  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-
 package co.bitshifted.reflex.core.config;
 
 public enum RFXRedirectPolicy {
